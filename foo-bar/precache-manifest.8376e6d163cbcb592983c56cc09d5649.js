54da30be91f0c117a66b4d78044b75e94626174b
Chris Jacob <i.chris.jacob@gmail.com>
2019-06-20 22:33:08 +1000

    Add files via upload

---
+++ foo-bar/precache-manifest.8376e6d163cbcb592983c56cc09d5649.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2a73940a5480a29e75662c98fb10b91",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0c0c461f1a0fa8438c7f",
+    "url": "./static/css/main.f9c67ae1.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "./static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "0c0c461f1a0fa8438c7f",
+    "url": "./static/js/main.2d75f2b3.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
